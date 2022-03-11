358e122d13cdcad293ae2e8fc1b2f8ff30b0d7bd
BenchSL <timzupanc@gmail.com>
2022-03-11 17:09:02 +0100

    Adding DetectSensorError window, calls to API, function to return random errCode, class Error.

---
+++ eGrow-simulator/Class/Error.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eGrow_simulator.Class
+{
+    public class Error
+    {
+        public string ErrorCode { get; set; }
+        public string ErrorNotification { get; set; }
+        public string IdSensor { get; set; }
+
+        public Error()
+        {
+
+        }
+
+        public Error(string errCode, string errNoty, string idSen)
+        {
+            ErrorCode = errCode;
+            ErrorNotification = errNoty;
+            IdSensor = idSen;
+        }
+
+        public override string ToString()
+        {
+            return $"Error code: {ErrorCode} | Error notification: {ErrorNotification} | IdSensor: {IdSensor}";
+        }
+    }
+}
---
+++ eGrow-simulator/DetectSensorError.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using eGrow_simulator.Class;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace eGrow_simulator
+{
+    /// <summary>
+    /// Interaction logic for DetectSensorError.xaml
+    /// </summary>
+    public partial class DetectSensorError : Window
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static async Task<List<Error>> GetErrors()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var stringError = await client.GetStreamAsync(""); //url call to API
+            var SeriaError = await JsonSerializer.DeserializeAsync<List<Error>>(stringError); //converting json to List<Error>
+            return SeriaError;
+        }
+
+        private static async Task<Error> GetRandomError(string errCode)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var stringError = await client.GetStreamAsync("" + errCode); //url call to API
+            var SeriaError = await JsonSerializer.DeserializeAsync<Error>(stringError); //converting json to List<Error>
+            return SeriaError;
+        }
+
+        private string GenerateRandomCode() //codes can be 404, 501, 604 or 705
+        {
+            string[] CodesArr = new string[4] { "404", "501", "604", "705"};
+            string randomCode = string.Empty;
+            Random rnd = new Random();
+            int index = rnd.Next(CodesArr.Length);
+            randomCode = CodesArr[index];
+            return randomCode;
+        }
+
+        public DetectSensorError()
+        {
+            InitializeComponent();
+        }
+    }
+}
